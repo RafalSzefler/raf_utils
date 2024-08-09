f4648199fd847bc47b23eaf467e5456436b0975c
Rafal Szefler <rafael.szefler@gmail.com>
2024-08-09 11:14:32 +0200

    Skeleton

---
+++ projects/raf_readonly/src/lib.rs
@@ -0,0 +1,16 @@
+#![warn(clippy::all, clippy::pedantic)]
+#![allow(
+    clippy::needless_return,
+    clippy::redundant_field_names,
+    clippy::unreadable_literal,
+    clippy::inline_always,
+    clippy::must_use_candidate,
+    clippy::module_name_repetitions,
+)]
+
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn readonly(input: TokenStream) -> TokenStream {
+    todo!()
+}
